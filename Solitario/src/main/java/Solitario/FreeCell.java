b5b8db6ef1f75037fbcaad89361222b6d11192a1
rodrigorotondo <rrotondo@fi.uba.ar>
2023-10-29 13:37:51 -0300

    ColumnaColumna FreeCell + Serializador

--- Solitario/src/main/java/Solitario/FreeCell.java
+++ Solitario/src/main/java/Solitario/FreeCell.java
@@ -39,8 +39,16 @@ public class FreeCell extends Solitario {
         this.iniciarAuxiliares();
         this.repartirCartas();
     }
-    public void jugadaColumnaAColumna(){ // hay que implementarla de otra manera para freecell
+    public void jugadaColumnaAColumna(int indiceColumnaDestino, int indiceColumnaOrigen, int indiceCartaOrigen){
+        ColumnaDeJuego columnaDestino = this.tablero[indiceColumnaDestino];
+        ColumnaDeJuego columnaOrigen = this.tablero[indiceColumnaOrigen];
+        int espaciosVacios = cantidadEspaciosVacios();
+        if(this.reglas.puedoAgregarCartasAColumna(columnaOrigen, columnaDestino, espaciosVacios)){
+            columnaOrigen.cambiarDeColumna(columnaDestino, indiceCartaOrigen);
+        }
     }
+
+
     public void jugadaAuxiliarAColumna(int indiceAuxiliar, int indiceColumnaDestino){
         Carta cartaAuxiliar = this.auxiliares[indiceAuxiliar].verUltimaCarta();
         Palo paloCarta = cartaAuxiliar.obtenerPalo();
@@ -66,6 +74,24 @@ public class FreeCell extends Solitario {
                 this.auxiliares[indiceAuxiliar].cambiarAStack(fundacionDestino);
             }
         }
+
+    private int cantidadEspaciosVacios(){
+        int espaciosVacios = 0;
+        for(int i = 0; i < this.CANTIDADDEAUXILIARES; i++){
+            if(auxiliares[i].estaVacia()){
+                espaciosVacios++;
+            }
+        }
+        for(int i = 0; i < this.CANTIDADDECOLUMNAS; i++){
+            if(tablero[i].estaVacia()){
+                espaciosVacios++;
+            }
+        }
+
+        return espaciosVacios;
+
+    }
+
     @Override
     public void juegoAPuntoDeGanarConCartaEnColumna() {
         while (!mazo.estaVacia()) {//tener mazo vacio
--- Solitario/src/main/java/Solitario/VisitorSerializador.java
+++ Solitario/src/main/java/Solitario/VisitorSerializador.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public interface VisitorSerializador {
     public abstract Klondike cargarEstadoKlondike(String nombreArchivo) throws IOException, ClassNotFoundException;
+    public abstract FreeCell cargarEstadoFreeCell(String nombreArchivo) throws IOException, ClassNotFoundException;
     public abstract void guardarEstado(Solitario solitario, String nombreArchivo) throws IOException;
 
 }
