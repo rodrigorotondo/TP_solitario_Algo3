8aef9791cfbd1cbbb3942cde92420c4263f08990
rodrigorotondo <rrotondo@fi.uba.ar>
2023-10-30 20:23:28 -0300

    unificación de tests

--- Solitario/src/test/java/KlondikeTest.java
+++ Solitario/src/test/java/KlondikeTest.java
@@ -1,18 +1,20 @@
 import Solitario.*;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
-public class klondiketest01 {
+public class KlondikeTest {
    @Test
-    public void klondikeInicializacionEnEstadoAPuntoDeGanar() {
+    public void klondikeInicializacionEnEstadoAPuntoDeGanar1() {
         FabricaDeSolitarios FabricaKlondike = new FabricaDeSolitariosKlondike();
         Solitario klondike = FabricaKlondike.crearSolitario();
         klondike.juegoAPuntoDeGanarConCartaEnColumna();
         assertFalse(klondike.juegoTerminado());
     }
     @Test
-    public void klondikeJuegoGanado() {
+    public void klondikeJuegoGanado1() {
         Klondike klondike = new Klondike();
         klondike.juegoAPuntoDeGanarConCartaEnColumna();
         klondike.jugadaColumnaAFundacion(0, 3);
@@ -42,5 +44,35 @@ public class klondiketest01 {
         klondike.jugadaColumnaAFundacion(3, 3);
         klondike.jugadaColumnaAFundacion(3, 3);
         assertTrue(klondike.juegoTerminado());
+    }
+
+    @Test
+    public void klondikeInicializacionEnEstadoAPuntoDeGanar2() {
+        Klondike klondike = new Klondike();
+        klondike.juegoAPuntoDeGanarConCartaEnDescarte();
+        assertFalse(klondike.juegoTerminado());
+    }
+    @Test
+    public void klondikeJuegoGanado2() {
+        Klondike klondike = new Klondike();
+        klondike.juegoAPuntoDeGanarConCartaEnDescarte();
+        klondike.jugadaDescarteFundacion(3);
+        assertTrue(klondike.juegoTerminado());
+    }
+    @Test
+    public void klondikeCargarJuego() throws IOException, ClassNotFoundException {
+        //arrange
+        Klondike klondike = new Klondike();
+        SerializadorSolitario serializadorSolitario = new SerializadorSolitario();
+        klondike.juegoAPuntoDeGanarConCartaEnColumna();
+        //act
+        //aca sabemos que si movemos una carta ganamos el juego, lo guardamos
+        klondike.guardarEstado("JuegoPrueba",serializadorSolitario);
+
+        Solitario klondikeCargado;
+        klondikeCargado = Klondike.cargarEstado("JuegoPrueba",serializadorSolitario);
+        klondikeCargado.jugadaColumnaAFundacion(0, 3);
+        //assert
+        assertTrue(klondikeCargado.juegoTerminado());
     }
 }
